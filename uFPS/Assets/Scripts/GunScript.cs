3c94ca70bb8d3c3f89abe828a1360f3e8ff5ffe9
XnoahR <rio.ae23@gmail.com>
2023-01-14 17:29:03 +0700

    Add some switching mechanic

--- uFPS/Assets/Scripts/GunScript.cs
+++ uFPS/Assets/Scripts/GunScript.cs
@@ -7,24 +7,37 @@ public class GunScript : MonoBehaviour
 [SerializeField] public float _Range;
 [SerializeField] public float _FireRate;
 [SerializeField] public float _BulletForce;
+private Rigidbody rb;
 public float _FireCooldown = 0;
 public Camera FpsCam;
 public ParticleSystem _MuzzleFlash;
 public GameObject _BulletImpact;
+private bool isUsed;
+private bool isDropped;
+
 
     // Start is called before the first frame update
 
     
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        isUsed = transform.parent != null;
+        if(isUsed)
+        {
+        isDropped = false;
+        rb.isKinematic =true;
         ShotInput();
-
+        }
+        else{
+        isDropped = true;
+        rb.isKinematic = false;
+        }
     }
 
     protected virtual void ShotInput(){
---
+++ uFPS/Assets/WeaponManager.cs
@@ -0,0 +1,56 @@
+
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+   public GameObject[] Weapons = new GameObject[3];
+    [SerializeField] int _CurrentIndex;
+    [SerializeField] int _PreviousIndex;
+    private void Start() {
+        _CurrentIndex = 0;
+        _PreviousIndex = _CurrentIndex;
+        SwitchWeapon();
+    }
+    private void Update() {
+        InputWeapon();
+        
+    }
+
+    void InputWeapon(){
+        _PreviousIndex = _CurrentIndex;
+
+        if(Input.GetKeyDown(KeyCode.Alpha1) && Weapons[0].transform.childCount >=1){
+            _CurrentIndex = 0;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2 )&& Weapons[1].transform.childCount >=1){
+            _CurrentIndex = 1;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3)&& Weapons[2].transform.childCount >=1){
+            _CurrentIndex = 2;
+        }
+        if(Input.GetKeyDown(KeyCode.G) && _CurrentIndex != 2){
+            Rigidbody Crb = Weapons[_CurrentIndex].transform.GetChild(0).transform.GetComponent<Rigidbody>();
+            Crb.AddForce(transform.forward*50f,ForceMode.Impulse);
+            Weapons[_CurrentIndex].transform.DetachChildren();
+            _CurrentIndex++;
+        }
+        if(_PreviousIndex != _CurrentIndex){
+            SwitchWeapon();
+        }
+    }
+
+    void SwitchWeapon(){
+        int i = 0;
+        foreach(GameObject x in Weapons){
+            if(i == _CurrentIndex){
+                x.gameObject.SetActive(true);
+            }
+            else{
+                x.gameObject.SetActive(false);
+            }
+        i++;
+        }
+
+
+    }
+}
